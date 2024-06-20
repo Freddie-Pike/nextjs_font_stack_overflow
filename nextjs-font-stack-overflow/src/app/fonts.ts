ec2fbab272551700446e16ec5f0c81e99f3c462e
Freddie-Pike <freddiepike709@gmail.com>
2024-06-19 23:41:51 -0230

    init NextJS app with Heebo and Inter.

---
+++ nextjs-font-stack-overflow/src/app/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Heebo } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"], display: "swap" });
+export const heebo = Heebo({ subsets: ['latin'], display: "swap" })
---
+++ nextjs-font-stack-overflow/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { heebo } from './fonts';
+
+export default function Home() {
+  return (
+    <>
+      <h1 className={`text-center ${heebo.className} white`}>Heebo</h1>
+      <h2 className='text-center white'>Roboto</h2>
+    </>
+  );
+}
